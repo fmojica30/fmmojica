48020926ad8e9eb6d30495f881987105d19eaa78
Fernando Martinez Mojica <fernando.mojica@utexas.edu>
2019-10-22 22:23:49 -0500

    Adding in burger animation

--- www/js/master.js
+++ www/js/master.js
@@ -1,10 +1,14 @@
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.menu');
+  const navLinks = document.querySelectorAll('.nav .menu .menuBtn');
 
-  burger.addEventListener('click', ()=>{
+  burger.addEventListener('click', () => {
     nav.classList.toggle('menu-active');
-  })
+    //burger animation
+    burger.classList.toggle('toggle');
+  });
+
 }
 
 navSlide();
